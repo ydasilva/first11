b63393740c7930fb71e3c12093be7d8e5c78b1e3
Youssouf-dev <youssouf@psyphertxt.com>
2018-11-09 18:02:21 +0000

    Support A Project

--- src/pages/get-involved/SupportAProject.js
+++ src/pages/get-involved/SupportAProject.js
@@ -10,7 +10,10 @@ class SupportAProject extends Component {
                 <div className="grid-container grid-x text-center">
                     <div className="spaceBar"/>
                     <div className="small-12">
-                        <h1>SupportAProject</h1>
+                        <h1>Educational Pathways</h1>
+                        <p>
+                        As a student approaches the age of fifteen at the Foundation they will be offered the choice to apply for one of our educational pathways. These include opportunities in the US, the UK or continued education within professional football. The PSF student-athlete pathway provides students the chance to enter top private high schools on full scholarships that will extend through to university. If students are pursuing a professional contract at eighteen, they will remain at the Foundation and continue their varied education, including football specific life courses, while in Ghana or on training attachment in Europe.
+                        </p>
                     </div>
                     
                 </div>
